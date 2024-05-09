0e21bdedd7d5f81e56d79503517c9e28f88ee923
Geme <90888249+GemeWolf@users.noreply.github.com>
2024-05-08 19:56:32 -0600

    Empezemos
    Proyecto piloto

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+fivem_checker 'yes'
+github 'https://github.com/BxRayven/nohatfalloff'
+version '2.0.0'
+name '^2No Hat Fall Off'
